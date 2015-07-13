200bab0c3f1399e58d0aab21b891d2fe788f1be5
Micah Cooper <mrmicahcooper+mrmicahcooper@gmail.com>
2015-07-13 18:33:54 -0400

    Expose stations and station

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,23 @@
+class PagesController < ApplicationController
+
+  before_action :set_headers
+
+  def stations
+    render json: Oj.dump( Noaa::Station.all.group_by(&:state_name), mode: :compat)
+  end
+
+  def station
+    station = Noaa::Station.find(params[:station_id])
+    tides = station.tides_for_date(params[:date])
+
+    render json: Oj.dump(tides.group_by{|t| t.tide}, mode: :compat)
+  end
+
+  private
+
+  def set_headers
+    headers["Access-Control-Allow-Origin" ] = "*"
+    headers["Access-Control-Request-Method"] = "GET"
+  end
+
+end
